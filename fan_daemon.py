05d505e668a49cb7ce8657d724e9609044b5f7b1
Ostheer <unconfigured@null.spigotmc.org>
2019-02-04 00:37:39 +0100

    First working version. Only forced values implemented

--- fan_daemon.py
+++ fan_daemon.py
@@ -9,8 +9,8 @@ CONNECTED = False
 #Config params
 baudrate = 9600
 serialroot = '/dev/ttyUSB'
-forecelevel1 = 0
-forecelevel2 = 0
+forcelevel1 = 0
+forcelevel2 = 0
 
 #Fan params
 sSetpoint1 = 0
@@ -20,13 +20,12 @@ sMeasure2 = 0
 
 #Functions
 def sendAndReceive(command):
-	print("ik poog te versturen: " + command)
 	ser.write(command.encode("utf-8"))
-	print("keepalive")
 	recv = ser.readline()
-	recv = recv.decode("utf-8")
-	print("Received in functie:")
-	print(recv)
+	try:
+		recv = recv.decode("utf-8")
+	except UnicodeDecodeError:
+		recv = 'non-utf-8'
 	return recv
 
 ## INITIALISATION
@@ -38,38 +37,42 @@ try:
 		
 	for line in lines:
 		keyword = line.split('=')[0].strip()
-		if not (keyword == ''):
-			if(keyword == 'baudrate'):
-				baudrate = int(line.split('=')[1].split('\n')[0].strip())
-			elif(keyword == 'serialroot'):
-				serialroot = line.split('=')[1].split('\n')[0].strip()
-			elif(keyword == 'forcelevel1'):
-				forecelevel1 = int(line.split('=')[1].split('\n')[0].strip())
-			elif(keyword == 'forcelevel2'):
-				forecelevel2 = int(line.split('=')[1].split('\n')[0].strip())
-			else:
+		if(keyword == 'baudrate'):
+			baudrate = int(line.split('=')[1].split('\n')[0].strip())
+		elif(keyword == 'serialroot'):
+			serialroot = line.split('=')[1].split('\n')[0].strip()
+		elif(keyword == 'forcelevel1'):
+			forcelevel1 = int(line.split('=')[1].split('\n')[0].strip())
+		elif(keyword == 'forcelevel2'):
+			forcelevel2 = int(line.split('=')[1].split('\n')[0].strip())
+		else:
+			if not (keyword == ''):
 				print("Unknown keyword: " + keyword)
+			
+	print("Configuration:")
+	print(str(serialroot) + '*')
+	print(str(baudrate) + ' baud')
+	print('force fan1: ' + str(forcelevel1))
+	print('force fan2: ' + str(forcelevel2))
+	print('')
+	
 except FileNotFoundError:
 	print("No configuration file! Using defaults...")
 finally:
 	#Open serial port
 	try:
 		devices = glob.glob(serialroot + "*")
+		devices.reverse()
 		
 		for device in devices:
-			print(devices)
-			print("Trying to connect to " + device + " with settings:")
-			print(device)
-			print(baudrate)
-			print(TIMEOUT)
+			print("Trying to connect to " + device + "...")
 			ser = serial.Serial(device,baudrate,timeout=TIMEOUT)
-			time.sleep(4)
+			time.sleep(3)
 			a = sendAndReceive('W')
 			print(a)
-			a = 'nietgoeed'
 			if(a.split('\r')[0] == '1337'):
 				#Device responds with appropriate ID
-				print("Connected with controller on " + device)
+				print("Connected to controller on " + device)
 				CONNECTED = True
 				break
 			else:
@@ -77,7 +80,6 @@ finally:
 				print(device + " replied with incorrect ID")
 				ser.close()
 				
-		print("ik zeg dat ik klaar ben met de for loop")
 	except serial.SerialException:
 		#dingen hier?
 		print("Unexpected serial error. Giving up.")
@@ -99,10 +101,11 @@ try:
 			print("1: temp monitor not implemented")
 		else:
 			if not (sSetpoint1 == forcelevel1):
+				print("Setting forcelevel1...")
 				try:
-					recv = sendAndReceive(str(forecelevel1) + 'O')
-					if (recv == 'f1:'+str(forecelevel1)):
-						sSetpoint1 = forecelevel1
+					recv = sendAndReceive(str(forcelevel1) + 'O')
+					if (recv == 'f1:'+str(forcelevel1)+'\r\n'):
+						sSetpoint1 = forcelevel1
 						print("Fan 1 speed set to forced level")
 				except serial.SerialException:
 					print("Oh fuck, something happened.")
@@ -112,11 +115,12 @@ try:
 		if (forcelevel2 == 0):
 			print("2: temp monitor not implemented")
 		else:
-			if not (sSetpoint2 == forcelevel2):				
+			if not (sSetpoint2 == forcelevel2):
+				print("Setting forcelevel2...")
 				try:
-					recv = sendAndReceive(str(forecelevel2) + 'T')
-					if (recv == 'f2:'+str(forecelevel2)):
-						sSetpoint2 = forecelevel2
+					recv = sendAndReceive(str(forcelevel2) + 'T')
+					if (recv == 'f2:'+str(forcelevel2)+'\r\n'):
+						sSetpoint2 = forcelevel2
 						print("Fan 2 speed set to forced level")
 				except serial.SerialException:
 					print("Oh fuck, something happened.")
